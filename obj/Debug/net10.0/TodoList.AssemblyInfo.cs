a32623f2dd66f62c68e2bc881f0c8d2709a05279
Lucas-Henrii <lucashenriquesantos1415@gmail.com>
2026-02-16 17:09:31 -0300

    Atualizando arquivo Dockerfile

--- obj/Debug/net10.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692863fa01cb48e3ec7676339e563ac57a3406df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb943949d63d4776054becfc8e94490fd3c33214")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
